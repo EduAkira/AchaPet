aac984673801737f20c6179e5c20d79acbd6c752
Danilo Fernandes de Aguiar <danilofernandesdeaguiar@gmail.com>
2019-05-04 22:55:44 -0300

    conexão com fire base ok, foi inserido e consultado dados

--- AchaPet_app/app/src/main/java/br/com/achapet/Activity/MainActivity.java
+++ AchaPet_app/app/src/main/java/br/com/achapet/Activity/MainActivity.java
@@ -1,14 +1,10 @@
 package br.com.achapet.Activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.achapet.R;
-import br.com.achapet.Util.PermissaoRecurso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,43 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final PermissaoRecurso permissaoRecurso = new PermissaoRecurso(MainActivity.this);
-
-        Button b1 = (Button) findViewById(R.id.camera);
-        Button b2 = (Button) findViewById(R.id.arquivo);
-        Button b3 = (Button) findViewById(R.id.gps);
-
-        final TextView t1 = (TextView) findViewById(R.id.textCamera);
-        final TextView t2 = (TextView) findViewById(R.id.textArquivo);
-        final TextView t3 = (TextView) findViewById(R.id.textGPS);
-
-        t1.setText(""+ permissaoRecurso.possoUsarCamera());
-        t2.setText(""+ permissaoRecurso.possoUsarArquvo());
-        t3.setText(""+ permissaoRecurso.possoUsarGPS());
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                permissaoRecurso.pedirPermissaoCamera();
-                t1.setText(""+ permissaoRecurso.possoUsarCamera());
-            }
-        });
-
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                permissaoRecurso.pedirPermissaoArquivo();
-                t2.setText(""+ permissaoRecurso.possoUsarArquvo());
-            }
-        });
-
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                permissaoRecurso.pedirPermissaoGps();
-                t3.setText(""+ permissaoRecurso.possoUsarGPS());
-            }
-        });
 
     }
 }
